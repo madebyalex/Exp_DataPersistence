6dcbfff1ea6f4f8c520db2276767e1eafbdf2172
Alexander <hello@alexander.works>
2020-11-28 02:03:34 +0200

    UserDefaults: Create UI, save and reset data

--- Exp_DataPersistence/ViewController.swift
+++ Exp_DataPersistence/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var fullNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        fullNameTextField.text = UserDefaults.standard.string(forKey: "fullName")
+        emailTextField.text = UserDefaults.standard.string(forKey: "email")
+        phoneTextField.text = UserDefaults.standard.string(forKey: "phone")
     }
+    
+    
 
-
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+        UserDefaults.standard.setValue(fullNameTextField.text, forKey: "fullName")
+        UserDefaults.standard.setValue(emailTextField.text, forKey: "email")
+        UserDefaults.standard.setValue(phoneTextField.text, forKey: "phone")
+    }
+    
+    @IBAction func clearDataButtonTapped(_ sender: UIBarButtonItem) {
+        clearData()
+    }
+    
+    func clearData() {
+        fullNameTextField.text = ""
+        emailTextField.text = ""
+        phoneTextField.text = ""
+        
+        UserDefaults.standard.removeObject(forKey: "fullName")
+        UserDefaults.standard.removeObject(forKey: "email")
+        UserDefaults.standard.removeObject(forKey: "phone")
+    }
+    
 }
 
